b89f4cc5e5c82f8f1906ffda77d18b2c0ac49175
emaddkhan <emadkhan.yousufzai@gmail.com>
2023-10-28 17:10:25 +0500

    My new layout

---
+++ minimalist-scrapbook-web-design-template/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primarybackground: "#e9e7e7",
+        primary: "#515155",
+        bordery: "#232323",
+        button1: "#232323",
+        paragraph: "#656464",
+       heading: "#232323",
+       background:"#232323",
+       paragraph2:"#8d8c8c",
+       paragraph3:"#c1c0c0",
+       footer:"#c4c4c4",
+      },
+    },
+  },
+  plugins: [],
+}
+
