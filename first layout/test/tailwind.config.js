4617095424c1c166880b2dae9e1e2fe4d7242a97
emaddkhan <emadkhan.yousufzai@gmail.com>
2023-10-28 16:42:31 +0500

    Marioclub Layout

---
+++ first layout/test/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      }
+    }
+  }
+}
